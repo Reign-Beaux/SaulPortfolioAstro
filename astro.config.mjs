a07bb3ebf771e89f21e778c5338b25732f186127
Saúl Antonio Morquecho Cela <samc920130@hotmail.com>
2025-10-21 23:37:28 -0600

    se agrega plugin para despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://saulantonio.br-y-sa.com',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
