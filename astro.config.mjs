c507769e640c6e0416da054a28aa08a0e366c690
Saúl Antonio Morquecho Cela <samc920130@hotmail.com>
2025-10-22 00:20:14 -0600

    se retira plugin de node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://saulantonio.br-y-sa.com',
-
-  adapter: node({
-    mode: 'standalone',
-  }),
 });
